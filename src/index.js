7d44568a817d039d92915d0e98fb0490f89ec3b6
Yasaman Abedi <Yasaman471@gmail.com>
2025-06-06 20:32:35 +0430

    chore: install Material UI, configure GraphQL and Apollo Client, and create schema in Hygraph

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri: 'https://us-west-2.cdn.hygraph.com/content/cma5bvoyi016q06w8ze6zpduv/master',
+  cache: new InMemoryCache(),
+});
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
