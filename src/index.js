8362350fbdc627cfc09b5981176eba963e9baa47
Yasaman Abedi <Yasaman471@gmail.com>
2025-06-11 07:00:49 +0430

    feat: Use YekanBakh font and customize Material UI theme
    - Integrates YekanBakh font for improved UI.
    - Configures font weights and RTL direction in theme.
    - Applies custom theme using ThemeProvider.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import { ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
-import './index.css';
+import { ThemeProvider } from '@emotion/react';
+import theme from './mui/theme';
+
 import App from './App';
+import './Styles/index.css';
+import "./Styles/fonts.css";
+
 
 const client = new ApolloClient({
   uri: 'https://us-west-2.cdn.hygraph.com/content/cma5bvoyi016q06w8ze6zpduv/master',
@@ -14,6 +20,8 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </ApolloProvider>
 );
